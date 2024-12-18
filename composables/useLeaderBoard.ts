b2b7b04338ad006905b5c1f1ba8e5a863077a4a3
toblinkz <tobi.egbayelo@termii.com>
2024-12-18 12:58:07 +0100

    update campaign tag

--- composables/useLeaderBoard.ts
+++ composables/useLeaderBoard.ts
@@ -1,6 +1,6 @@
 export const getLeaderBoard = async () => {
   let response = (await useApiFetch(
-    "/playlog/campaigns/leaderboards?campaign=CAMPAIGN_GROWING_UP&limit=20"
+    "/playlog/campaigns/leaderboards?campaign=CAMPAIGN_GROWING_UP_TWO&limit=20"
   )) as unknown as any;
   return parseDataFromApi(response.data);
 };
