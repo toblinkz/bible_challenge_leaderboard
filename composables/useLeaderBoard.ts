24068f9ed2dee26c58c18d0c91025a3e104bf9ac
toblinkz <tobi.egbayelo@termii.com>
2024-12-18 23:27:57 +0100

    update

--- composables/useLeaderBoard.ts
+++ composables/useLeaderBoard.ts
@@ -1,7 +1,7 @@
 export const getLeaderBoard = async () => {
   let response = (await useApiFetch(
     
-    "/playlog/campaigns/leaderboards?campaign=CAMPAIGN_GROWING_UP_TWO&limit=20"
+    "/playlog/campaigns/leaderboards?campaign=CAMPAIGN_GROWING_UP_THREE&limit=20"
   )) as unknown as any;
 
   return parseDataFromApi(response.data);
